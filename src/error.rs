f7232547b6a2a0bd8a6196afbad44adbf7e6dd12
Maxim Zhukov <mussitantesmortem@gmail.com>
2020-07-21 23:57:38 +0300

    Remove walkdir
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -3,10 +3,16 @@ pub(crate) type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug)]
 pub(crate) enum Error {
     GitError(git2::Error),
+    IOError(std::io::Error),
 }
 
 impl From<git2::Error> for Error {
     fn from(e: git2::Error) -> Self {
         Error::GitError(e)
+    }
+}
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::IOError(e)
     }
 }
