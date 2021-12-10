d86bc2376ad906decc6d3b059242442c47f7a375
Maxim Zhukov <mussitantesmortem@gmail.com>
2021-12-10 16:25:55 +0300

    src: update to clap rc3
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -14,160 +14,58 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-use clap::{crate_version, App, Arg, ColorChoice};
+use clap::Parser;
 use regex::Regex;
-use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-#[derive(Clone)]
+#[derive(Clone, Parser, Debug)]
+#[clap(about, version, author)]
 pub(crate) struct Options {
+    /// Show the status of all branches
+    #[clap(short, long)]
     pub(crate) all_branch: bool,
+
+    /// Show debug message
+    #[clap(long)]
     pub(crate) debug: bool,
+
+    /// Display info only when repository needs action
+    #[clap(short, long)]
     pub(crate) quiet: bool,
+
+    /// Force remote update (slow)
+    #[clap(short, long)]
     pub(crate) remote: bool,
+
+    /// Show untracked files
+    #[clap(short, long)]
     pub(crate) untracked: bool,
+
+    /// ignore branches matching the regex <re>
+    #[clap(short, long)]
     pub(crate) ignore_branch_regex: Option<Regex>,
+
+    /// Search <dir> for repositories (can be used multiple times)
+    #[clap(short = 'd', long = "dirs")]
     pub(crate) working_directories: Vec<PathBuf>,
 
+    /// Limit the depth of repositories search
+    #[clap(short, long)]
     pub(crate) max_depth: usize,
+
+    /// Specifies  the  number  of jobs (commands) to run simultaneously. (Default $(nprocs))
+    #[clap(short, long, default_value_t = num_cpus::get())]
     pub(crate) jobs: usize,
 }
 
 pub(crate) fn parse_args() -> Options {
-    let matches = App::new("gitcheck-rs")
-        .version(crate_version!())
-        .author("Maxim Zhukov")
-        .color(ColorChoice::Auto)
-        .about("rust gitcheck. Check multiple git repository in one pass")
-        // .arg(
-        //     Arg::new("verbose")
-        //         .short('v')
-        //         .long("verbose")
-        //         .about("Show files & commits"),
-        // )
-        .arg(
-            Arg::new("debug")
-                .long("debug")
-                .about("Show debug message"),
-        )
-        .arg(
-            Arg::new("remote")
-                .short('r')
-                .long("remote")
-                .about("force remote update (slow)"),
-        )
-        .arg(
-            Arg::new("untracked")
-                .short('u')
-                .long("untracked")
-                .about("Show untracked files"),
-        )
-        // .arg(
-        //     Arg::new("bell")
-        //         .short('b')
-        //         .long("bell")
-        //         .about("bell on action needed"),
-        // )
-        // .arg(
-        //     Arg::new("watch")
-        //         .short('w')
-        //         .long("watch")
-        //         .value_name("sec")
-        //         .takes_value(true)
-        //         .about("after displaying, wait <sec> and run again"),
-        // )
-        .arg(
-            Arg::new("ignore-branch")
-                .short('i')
-                .long("ignore-branch")
-                .value_name("re")
-                .takes_value(true)
-                .about("ignore branches matching the regex <re>"),
-        )
-        .arg(
-            Arg::new("dir")
-                .short('d')
-                .long("dir")
-                .value_name("dir")
-                .takes_value(true)
-                .multiple_values(true)
-                .about("Search <dir> for repositories (can be used multiple times)"),
-        )
-        .arg(
-            Arg::new("maxdepth")
-                .short('m')
-                .long("maxdepth")
-                .value_name("maxdepth")
-                .takes_value(true)
-                .about("Limit the depth of repositories search"),
-        )
-        .arg(
-            Arg::new("quiet")
-                .short('q')
-                .long("quiet")
-                .about("Display info only when repository needs action"),
-        )
-        // .arg(
-        //     Arg::new("email")
-        //         .short('e')
-        //         .long("email")
-        //         .about("Send an email with result as html, using mail.properties parameters"),
-        // )
-        .arg(
-            Arg::new("all-branch")
-                .short('a')
-                .long("all-branch")
-                .about("Show the status of all branches"),
-        )
-        // .arg(
-        //     Arg::new("localignore")
-        //         .short('l')
-        //         .long("localignore")
-        //         .value_name("re")
-        //         .takes_value(true)
-        //         .about("ignore changes in local files which match the regex <re>"),
-        // )
-        // .arg(
-        //     Arg::new("init-email").long("init-email").about(
-        //         "Initialize mail.properties file (has to be modified by user using JSON Format",
-        //     ),
-        // )
-        .arg(
-            Arg::new("jobs")
-                .short('j')
-                .long("jobs")
-                .value_name("jobs")
-                .takes_value(true)
-                .about("Specifies  the  number  of jobs (commands) to run simultaneously. (Default $(nprocs))"),
-        )
-        .get_matches();
+    let mut options = Options::parse();
 
-    Options {
-        all_branch: matches.is_present("all-branch"),
-        debug: matches.is_present("debug"),
-        quiet: matches.is_present("quiet"),
-        remote: matches.is_present("remote"),
-        untracked: matches.is_present("untracked"),
-
-        ignore_branch_regex: matches
-            .value_of("ignore-branch")
-            .and_then(|v| Regex::new(v).ok()),
-
-        working_directories: matches
-            .values_of("dir")
-            .map(|values| {
-                values
-                    .map(|d| Path::new(d).canonicalize().unwrap())
-                    .collect()
-            })
-            .unwrap_or_else(|| vec![env::current_dir().unwrap()]),
-        max_depth: matches
-            .value_of("maxdepth")
-            .and_then(|m| m.parse().ok())
-            .unwrap_or(usize::MAX),
-        jobs: matches
-            .value_of("jobs")
-            .and_then(|j| j.parse().ok())
-            .unwrap_or_else(num_cpus::get),
+    if options.working_directories.is_empty() {
+        options
+            .working_directories
+            .push(std::env::current_dir().unwrap());
     }
+
+    options
 }
